dbe779cce1e8b4e9ed3a550b4bfb8ad081cfeb19
FabianTamas <fabiant0130@gmail.com>
2020-09-11 10:59:02 +0200

    Megszámolás tétele

--- ProgramozasiTetelek/Program.cs
+++ ProgramozasiTetelek/Program.cs
@@ -29,11 +29,24 @@ namespace ProgramozasiTetelek
             Console.WriteLine("A tömb elemeinek összege: {0}", osszeg);
 
         }
+
+        static void Megszamolas()
+        {
+            int db = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i]%3 == 0)
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine("3-al osztható számok: {0}", db);
+        }
         static void Main(string[] args)
         {
             TombKiiras();
-
             Osszegzes();
+            Megszamolas();
 
             Console.ReadKey();
         }
